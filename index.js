b9a0ce8ff503f7ced75f5bbabaf3552a2c15f655
Eugenio Pace <eugeniop@auth0.com>
2016-11-08 15:36:40 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var request = require('request');
 
 module.exports = function(context,cb){
   
-  reques.post('https://hooks.slack.com/services/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',{
+  request.post('https://hooks.slack.com/services/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',{
     json: {
       text: '```\n' + JSON.stringify(context.data) + '\n```'
     }
