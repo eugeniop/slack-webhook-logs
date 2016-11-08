dcde5f1cb5f9ba0e121589cd9cea11541d6e3a07
Eugenio Pace <eugeniop@auth0.com>
2016-11-08 15:40:01 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = function(context,cb){
   
   request.post('https://hooks.slack.com/services/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',{
     json: {
-      text: '```\n' + JSON.stringify(context '\n```'
+      text: '```\n' + JSON.stringify(context.data) + '\n```'
     }
   },function(e,s,b){
     console.log('Error', e);
