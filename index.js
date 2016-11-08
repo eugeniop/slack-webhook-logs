89c47f3878f6adbae72350df2287408012051399
Eugenio Pace <eugeniop@auth0.com>
2016-11-08 15:43:18 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var request = require('request');
 
 module.exports = function(context,cb){
 
+  console.log(context);
+
+  if(context.params.apikey !== '123456789') return cb('Invalid API Key');
   
   request.post('https://hooks.slack.com/services/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',{
     json: {
